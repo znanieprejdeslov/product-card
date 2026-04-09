54d6b92f38a8999c5bad3a3c1b82a6bcb175a173
Nazim Nizamov <nizamovnk@gmail.com>
2026-04-10 00:53:02 +0500

    fix bags

--- homewrk-6.js
+++ homewrk-6.js
@@ -29,87 +29,86 @@ function checkMaxSpeed (car) {
 }
 
 //6. Написать функцию, которая получает первым аргументом  — объект, а вторым аргументом — свойство объекта, которое нужно вывести и выводит его значение.
-const getPropertyValue = (obj, key) => {
+const getObjectValue = (obj, key) => {
   console.log(obj[key]);
 }
 
-getPropertyValue(car,'brand');
+getObjectValue(car,'brand');
 
 //7. Создать массив, который содержит названия продуктов (просто строки)
 const vegetables = ['Тыква', 'огурец', 'помидор', 'картошка', 'баклажан'];
 
 //8. Создать массив, состоящий из объектов, где объект представляет собой книгу (название, автор, год выпуска, цвет обложки, жанр) (3-5 книг).
-const allBooks = [{
+const allBooks = [
+  {
   'name': 'Белый Клык',
   'author': 'Джек Лондон',
-  'year': '1906',
+  'year': 1906,
   'color': 'Зеленая'
   },
-
   {
     'name': 'Зов Предков',
   'author': 'Джек Лондон',
-  'year': '1903',
+  'year': 1903,
   'color': 'красная'
   },
-
   {
     'name': 'Бегущий за Ветром',
   'author': 'Халед Хоссейни',
-  'year': '2003',
+  'year': 2003,
   'color': 'синяя'
   },
-  
   {
     'name': 'Тысяча сияющий солнц',
   'author': 'Халед Хоссейни',
-  'year': '2007',
+  'year': 2007,
   'color': 'желтая'
-}];
+}
+];
 
-allBooks.push({
+allBooks.push(
+  {
   'name': 'Оно',
   'author': 'Стивен Кинг',
-  'year': '1986',
+  'year': 1986,
   'color': 'черная',
-});
+  }
+);
 
-const garryPotterMultiverse = [{
+const garryPotterMultiverse = [
+  {
   'name': 'Гарри Поттер и Философский камень',
   'author': 'Дж. К. Роулинг',
-  'year': '1997',
+  'year': 1997,
   'color': 'Зеленая'},
-
   {
     'name': 'Гарри Поттер и Тайная Комната',
   'author': 'Дж. К. Роулинг',
-  'year': '1998',
+  'year': 1998,
   'color': 'красная'
   },
-
   {
-    'name': 'Гарри Поттер и узник Азкабана',
+  'name': 'Гарри Поттер и узник Азкабана',
   'author': 'Дж. К. Роулинг',
-  'year': '1999',
+  'year': 1999,
   'color': 'синяя'
   },
-  
   {
-    'name': 'Гарри Поттер и Кубок Огня',
+  'name': 'Гарри Поттер и Кубок Огня',
   'author': 'Дж. К. Роулинг',
-  'year': '2000',
+  'year': 2000,
   'color': 'желтая'
-}]
-
+  }
+]
 
 const books = [ ...allBooks, ...garryPotterMultiverse];
 
 //10. Почитать про метод массива — map. Написать функцию, которая принимает массив сущностей с задания №9. 
-const isRareBook = (rareBooks) => {
-  return rareBooks.map(book => ({
-         ...book, 
-         isRare: Number(book.year) > 1997
-        }));
+const rareBooks = (books) => {
+  return books.map(book => ({
+    ...book, 
+    isRare: (book.year) > 1997
+  }));
 };
 
-console.log(isRareBook(books));
+console.log(rareBooks(books));
